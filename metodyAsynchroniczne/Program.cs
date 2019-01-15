095115bd768776fc2714c1e433da6b0a3228f423
Marian Marian <fenrir55@gmail.com>
2019-01-15 20:09:23 +0100

    v1

--- metodyAsynchroniczne/Program.cs
+++ metodyAsynchroniczne/Program.cs
@@ -10,6 +10,30 @@ namespace metodyAsynchroniczne
     {
         static void Main(string[] args)
         {
+            Asynchorniczne asynch = new Asynchorniczne();
+            asynch.wypiszDane();
+            Console.ReadKey();
+        }
+
+       
+    }
+    public class Asynchorniczne
+    {
+        public async void wypiszDane()
+        {
+            int wynikDzialania = wynik(3, 4);
+            Console.WriteLine($"Wynik działania to {wynikDzialania}");
+            int wynikDzialania2 = await wynikAsync(5, 7);
+            Console.WriteLine($"Wynik działania to {wynikDzialania2}");
+        }
+
+        int wynik(int a, int b)
+        {
+            return a * b;
+        }
+        Task<int> wynikAsync(int a, int b)
+        {
+            return Task.Run(() => a * b);
         }
     }
 }
